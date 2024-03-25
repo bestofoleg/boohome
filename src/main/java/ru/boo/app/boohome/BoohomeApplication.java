d7481b9f19a0b9352669012ab22c96f690017d2d
bestofoleg <olegabramov1998@gmail.com>
2024-03-25 21:03:50 +0400

    Начальный Spring шаблон добавлен.

---
+++ src/main/java/ru/boo/app/boohome/BoohomeApplication.java
@@ -0,0 +1,13 @@
+package ru.boo.app.boohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoohomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoohomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/boo/app/boohome/BoohomeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boo.app.boohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoohomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
