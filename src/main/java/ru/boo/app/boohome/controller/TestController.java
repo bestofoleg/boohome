7a85f75f2e8e46c06015616da8053ba9e23239dc
bestofoleg <olegabramov1998@gmail.com>
2024-04-18 16:57:48 +0400

    Добавил тестовый маппинг на Алису

--- src/main/java/ru/boo/app/boohome/controller/TestController.java
+++ src/main/java/ru/boo/app/boohome/controller/TestController.java
@@ -1,13 +1,26 @@
 package ru.boo.app.boohome.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.boo.app.boohome.dto.TestDto;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class TestController {
     @GetMapping("/test")
     public TestDto getTest() {
         return new TestDto("Hello, World with Boohome!");
+    }
+
+    @PostMapping("/test/alice")
+    public ResponseEntity<Void> testAlice(@RequestBody Map<String, Object> aliceRequest) {
+        log.info("Получено сообщения с сервера Яндекс через Алису: {} ", aliceRequest);
+        return ResponseEntity.noContent().build();
     }
 }
